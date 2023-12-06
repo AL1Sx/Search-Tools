dd8aa3f14c638dfbed5bacdad7dd37ec77461d12
Nekona Alice <125648566+AL1Sx@users.noreply.github.com>
2023-12-06 23:13:01 +0800

    PixSearch B2.1
    更新日志:
    1.修改了一些代码,删除了翻译功能;
    2.现已支持PID跳转.

--- files/PixSearch.py
+++ files/PixSearch.py
@@ -6,52 +6,48 @@
 #TODO pixnovelid定向
 import webbrowser
 import time
-from googletrans import Translator
 
-# 创建一个 Translator 对象
-translator = Translator()
-
-print("PixSearch\n版本号:Beta1\n请在出现[>>>]后再键入相关命令.\n---")
+print("PixSearch\n版本号:Beta2.1\n请在出现[>>>]后再键入相关命令.\n---")
 
 while True:
     print('选择你所需要的图床')
     print('[P]Pixiv.net')
+    print('[Q]退出')
     print('===')
     insert = input('图床选择>>>')
     if 'P' in insert or 'p' in insert:
         print('选择你需要的功能(Pixiv.net)')
-        print('[1]Pixiv基础Tag搜索')
+        print('[1]Tag标签搜索')
+        print('[2]PID定向跳转')
+        #print('[2X]PID第一张原图')
+        #print('[3]用户UID定向跳转')
+        #print('[4]Pixiv小说搜索')
+        #print('[5]Pixiv小说定向')
         print('[Q]返回')
         print('===')
         choice = input('功能选择>>>')
 
         while True:
-            if choice == '1':
+            if choice == '1':#TagSearch
                 search_word = input('关键词>>>')
                 if search_word == 'QUIT':
                     break
-                if search_word.endswith('/jp'):
-                    # 如果是，去掉 "/jp" 并翻译为日文
-                    search_word = search_word[:-3]
-                try:
-                    translated = translator.translate(search_word, dest='ja')
-                    search_word = translated.text
-                except AttributeError:
-                    print("翻译失败，将使用原词")
                 url = ('https://www.pixiv.net/tags/' + search_word + '/artworks?s_mode=s_tag&ai_type=1')
                 webbrowser.open_new_tab(url)
-                print('正在打开' + url)
+                print('正在打开' + url)   
+            elif choice == '2':#PID
+                search_word = input('PID>>>')
+                if search_word == 'QUIT':
+                    break
+                url = ('https://www.pixiv.net/artworks/' + search_word)
+                webbrowser.open_new_tab(url)
             elif 'Q' in choice or 'q' in choice:
                 break
 
-            elif 'Q' in insert or 'q' in insert:
-                print('即将在2秒后退出,[Enter]取消退出(暂时禁用)')
-                #if input().strip() == '':
-                #    print('已取消退出\n---')
-                #    continue
-            #else:
-                time.sleep(2)
-                break
+    elif 'Q' in insert or 'q' in insert:
+        print('即将在2秒后退出')
+        time.sleep(2)
+        break
 
     else:
         print('错误的')
