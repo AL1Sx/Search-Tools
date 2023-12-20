35ee57cec3cde5a5ca3edb5f6c8d026279abbbdf
Nekona Alice <125648566+AL1Sx@users.noreply.github.com>
2023-12-20 15:29:35 +0800

    v0.1.1 released
    更新内容:
    1.混淆跳转;
    2.update代码

--- files/PixSearch.py
+++ files/PixSearch.py
@@ -1,13 +1,10 @@
-#TODO pixid定向查找
-#TODO pixtag搜索
-#TODO pix搜索
-#TODO pixuid搜索
 #TODO pixnovel搜索
 #TODO pixnovelid定向
 import webbrowser
 import time
+import re
 
-print("PixSearch\n版本号:Beta3\n请在出现[>>>]后再键入相关命令.\n---")
+print("PixSearch\n版本号:v0.1.1\n请在出现[>>>]后再键入相关命令.\n---")
 
 while True:
     print('选择你所需要的图床')
@@ -20,6 +17,7 @@ while True:
         print('[1]Tag标签搜索')
         print('[2]PID定向跳转')
         #print('[2X]PID第一张原图')
+        print('[2H]PID混淆跳转 /*比如文字内包含神秘数字')
         print('[3]用户UID定向跳转')
         #print('[4]Pixiv小说搜索')
         #print('[5]Pixiv小说定向')
@@ -33,22 +31,39 @@ while True:
                     break
                 url = f'https://www.pixiv.net/tags/{search_word}/artworks?s_mode=s_tag&ai_type=1'
                 webbrowser.open_new_tab(url)
-                print('正在打开' + url)   
+                print('正在打开:' + url)   
+            
             elif choice == '2':#PID
                 search_word = input('PID>>>')
                 if search_word == 'QUIT':
                     break
                 url = f'https://www.pixiv.net/artworks/{search_word}'
                 webbrowser.open_new_tab(url)
+                print('正在打开:' + url) 
+            
+            elif '2H' in choice or '2h' in choice:#PID混淆
+                user_input = input('输入文字>>>')
+                if user_input == 'QUIT':
+                    break
+                only_numbers = re.findall(r'\d+', user_input)
+                search_word = ''.join(only_numbers)
+                url = f'https://www.pixiv.net/artworks/{search_word}'
+                webbrowser.open_new_tab(url)
+                print('正在打开:' + url)
+            
             elif choice == '3':#UID
                 search_word = input('UID>>>')
                 if search_word == 'QUIT':
                     break
                 url = f'https://www.pixiv.net/users/{search_word}'
                 webbrowser.open_new_tab(url)
-                print('正在打开' + url)
+                print('正在打开:' + url)
+            
             elif 'Q' in choice or 'q' in choice:
                 break
+
+            else:
+                continue
                              
     elif 'Q' in insert or 'q' in insert:
         print('即将在2秒后退出')
